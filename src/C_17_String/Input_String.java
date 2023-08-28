ba92ad9e91ee3d60deee5640bce273c311db96b7
Harsh91400 <harshit91400@gmail.com>
2023-08-28 18:16:05 +0530

    first commit

---
+++ src/C_17_String/Input_String.java
@@ -0,0 +1,22 @@
+package C_17_String;
+
+
+import java.util.Scanner;
+
+public class Input_String {
+    public static void main(String[] args) {
+//        Scanner sc= new Scanner(System.in);
+//        System.out.print("Enter Your Name : ");
+//        String name = sc.nextLine();
+//        System.out.println(name);
+
+
+        // Concatenation
+
+        String firstName="Harshit";
+        String lastName="Tripathi";
+        String fullName=firstName+" "+lastName;
+        System.out.println(fullName + " is this ");
+        System.out.println(firstName.charAt(0));
+    }
+}
--- src/LeetCode/Spiral_Matrix.java
+++ src/LeetCode/Spiral_Matrix.java
@@ -1,8 +1,50 @@
-package LeetCode;
-
-public class Spiral_Matrix {
-    public static void printSpiral(int arr[][]){
-
-    }
-    
-}
+//package LeetCode;
+//
+//import java.util.ArrayList;
+//
+//public class Spiral_Matrix {
+//    public static int printSpiral(int arr[][]){
+//        int row=arr.length;
+//        int col=arr[0].length;
+//        List <Integer> list= new ArrayList();
+//
+//        if(row==0) return list;
+//        int l=0,r=col-1,t=0,b=row-1,d=0;
+//
+//        while(l<=r && t<=b){
+//            if(d==0){
+//                for(int i=l;i<=r;i++){
+//                    list.add(arr[t][i]);
+//                }
+//                d=1;t++;
+//            }
+//            else if(d==1){
+//                for (int i = t; i <=b; i++) {
+//                    list.add(arr[i][r]);
+//                }
+//                d=2;r--;
+//            } else if (d==2) {
+//                for (int i = r; i >=l ; i--) {
+//                    list.add(arr[b][i]);
+//                }
+//                d=3;b--;
+//            } else if (d==3) {
+//                for (int i = b; i >=t ; i--) {
+//                    list.add(arr[i][l]);
+//                }
+//                d=0;l++;
+//            }
+//        }
+//        return list;
+//
+//
+//
+//
+//    }
+//
+//    public static void main(String[] args) {
+//
+//    }
+//
+//
+//}
